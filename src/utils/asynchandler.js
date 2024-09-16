4090ad9210f6885c2583ab4567269f3040c136f3
puru <percyjacker2002@gmail.com>
2024-09-16 12:08:56 +0530

    utils

---
+++ src/utils/asynchandler.js
@@ -0,0 +1,18 @@
+//method banke export krte h idhar
+
+//!method1 for async handler
+//function ke andar ek aur function and a callback{}
+const asynchandler= (fn)=>{async()=>{
+    try {
+        await fn(req,res,next)
+    } catch (error) {
+
+        res.status(err.code || 500).json({
+            success: false,
+            message: err.message
+        })
+    }
+}}
+
+export {asynchandler}
+
